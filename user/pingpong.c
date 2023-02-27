0331d0e161833aa4e577c1f4007fea12af90214e
Kr Cen <2256671169@qq.com>
2023-02-27 14:32:22 +0800

    pingpong.c done

--- user/pingpong.c
+++ user/pingpong.c
@@ -10,3 +10,56 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+// int pipe(int p[]): Create a pipe, put read/write fd in p[0], p[1]
+// int fork(): Create a process, return child's PID
+// int getpid(): Return the current process's PID
+// int read(int fd, char *buf, int n): 
+//      Read n bytes into buf; returns number read, or 0 if EOF
+
+// pipe: 0 read, 1 write
+
+int
+main(int argc, char *argv[])
+{
+  if(argc > 1){
+    printf("Err: Too much argument!\n");
+    exit(1);
+  }
+
+  int fat_p[2], kid_p[2];
+  char c, msg = 'x';
+
+  if(pipe(fat_p) < 0) {
+    printf("Err: Fail to initialize pipe!\n");
+    exit(1);
+  }
+  if(pipe(kid_p) < 0) {
+    printf("Err: Fail to initialize pipe!\n");
+    exit(1);
+  }
+
+  if(fork() == 0) { // parent -> child
+    if(read(fat_p[0], &c, 1) != 1) {
+        printf("Err: pipe read failed\n");
+        exit(1);
+    }
+    printf("%d: received ping\n", getpid());
+    if(write(kid_p[1], &msg, 1) != 1) {
+        printf("Err: pipe write failed\n");
+        exit(1);
+    }
+  }
+  else { // child -> parent
+    if(write(fat_p[1], &msg, 1) != 1) {
+        printf("Err: pipe write failed\n");
+        exit(1);
+    }
+    if(read(kid_p[0], &c, 1) != 1) {
+        printf("Err: pipe read failed\n");
+        exit(1);
+    }
+    printf("%d: received pong\n", getpid());
+  }
+  
+  exit(0);
+}
